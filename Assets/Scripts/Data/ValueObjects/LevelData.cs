c3b02767b9d1d52d180cf111e7c487b1e060fe9d
kaanuyann@gmail.com <kaanuyann@gmail.com>
2022-10-31 19:17:55 +0300

    no message

--- Assets/Scripts/Data/ValueObjects/LevelData.cs
+++ Assets/Scripts/Data/ValueObjects/LevelData.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 [Serializable]
 public class LevelData
     {
-        public List<PoolData> poolList = new List<PoolData>();
+        public List<PoolData> poolList = new List <PoolData>();
         
     }
 [Serializable]
