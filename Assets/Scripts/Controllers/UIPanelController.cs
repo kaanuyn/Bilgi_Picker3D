54a018da1d0bf95990f02b37aac4ee00e796df3d
kaanuyann@gmail.com <kaanuyann@gmail.com>
2022-11-28 21:15:25 +0300

    28.11.2022 Online Ders - UI Signals - Midterm Dahil Değil

--- Assets/Scripts/Controllers/UIPanelController.cs
+++ Assets/Scripts/Controllers/UIPanelController.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+    using System.Linq;
+using Signals;
 using UnityEngine;
 using Sirenix.OdinInspector;
 
@@ -8,7 +10,7 @@ public class UIPanelController : MonoBehaviour
 
     #region Serialized Variables
 
-    [SerializeField] private List<Transform> layers = new List <Transform> ();
+    [SerializeField] private List<Transform> layers = new List <Transform>();
 
     #endregion
     
@@ -22,35 +24,41 @@ public class UIPanelController : MonoBehaviour
 
     private void SubscribeEvents()
     {
-        
+        CoreUISignals.Instance.onOpenPanel += OnOpenPanel;
+        CoreUISignals.Instance.onClosePanel += OnClosePanel;
+        CoreUISignals.Instance.onCloseAllPanels += OnCloseAllPanels;
     }
     private void UnSubscribeEvents()
     {
-        
+        CoreUISignals.Instance.onOpenPanel -= OnOpenPanel;
+        CoreUISignals.Instance.onClosePanel -= OnClosePanel;
+        CoreUISignals.Instance.onCloseAllPanels -= OnCloseAllPanels;
     }
 
     private void OnDisable()
     {
         UnSubscribeEvents();
     }
-[Button("OpenPanel")]
+    //[Button("OpenPanel")]
     private void OnOpenPanel(UIPanelTypes type, int layerPos)
     {
         Instantiate(Resources.Load<GameObject>($"Screens/{type}Panel"), layers[layerPos]);
     }
-    [Button("ClosePanel")]
+    //[Button("ClosePanel")]
     private void OnClosePanel(int layerPos)
     {
-        if (layers[layerPos].transform.childCount > 0) 
+        if (layers[layerPos].transform.childCount > 0)
+        {
             Destroy(layers[layerPos].GetChild(0).gameObject);
+        }
+
     }
 
     private void OnCloseAllPanels()
     {
-        for (int i = 0; i < layers.Count; i++)
+        foreach (var t in layers.Where(t => t.childCount > 0))
         {
-            if (layers[i].transform.childCount > 0) 
-                Destroy(layers[i].GetChild(0).gameObject);
+            Destroy(t.GetChild(0).gameObject);
         }
     }
 }
--- Assets/Scripts/Signals/CoreGameSignals.cs
+++ Assets/Scripts/Signals/CoreGameSignals.cs
@@ -28,6 +28,7 @@ public UnityAction onLevelFailed = delegate {  };
 public UnityAction onLevelSuccessful = delegate {  };
 public UnityAction onNextLevel = delegate {  };
 public UnityAction onRestartLevel = delegate {  };
+public UnityAction onPlay = delegate {  };
 public UnityAction onReset = delegate {  };
 
 }
